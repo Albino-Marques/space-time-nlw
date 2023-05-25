832050356014e2c27df99a13f8e1720f621a9478
Albino Marques <albinomarques01@gmail.com>
2023-05-24 22:11:10 -0300

    Início da componentização do Front e início da autenticação no front e back.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,6 +11,8 @@ app.register(cors, {
   // origin: ['http://localhost:3000'],
   origin: true,
 })
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
